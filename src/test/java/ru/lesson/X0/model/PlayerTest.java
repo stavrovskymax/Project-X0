566f156efd5bdf88e62b8df43890d380be9e5ee0
stavrovskymax <stavrovsky96@yandex.ru>
2018-09-19 21:14:59 +0300

    Figure - created

---
+++ src/test/java/ru/lesson/X0/model/PlayerTest.java
@@ -0,0 +1,16 @@
+package ru.lesson.X0.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PlayerTest {
+
+    @Test
+    public void getName() {
+    }
+
+    @Test
+    public void getFigure() {
+    }
+}
