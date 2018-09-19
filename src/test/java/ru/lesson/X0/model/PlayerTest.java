19620d3f2ddd7864251f1f08a34f5586271ed7b1
stavrovskymax <stavrovsky96@yandex.ru>
2018-09-19 22:35:52 +0300

    PlayerTest - created

--- src/test/java/ru/lesson/X0/model/PlayerTest.java
+++ src/test/java/ru/lesson/X0/model/PlayerTest.java
@@ -7,10 +7,26 @@ import static org.junit.Assert.*;
 public class PlayerTest {
 
     @Test
-    public void getName() {
+    public void testGetName() {
+        final String inputValue = "Max";
+        final String expectedValue = inputValue;
+
+        final Player player = new Player(inputValue, null);
+
+        final String actualValue = player.getName();
+
+        assertEquals(expectedValue, actualValue);
     }
 
-    @Test
-    public void getFigure() {
-    }
+    /*@Test
+    public void testGetFigure() {
+        final Figure inputValue = Figure;
+        final Figure expectedValue = inputValue;
+
+        final Player player = new Player(null, inputValue);
+
+        final Figure actualValue = player.getFigure();
+
+        assertEquals(expectedValue, actualValue);
+    }*/
 }
