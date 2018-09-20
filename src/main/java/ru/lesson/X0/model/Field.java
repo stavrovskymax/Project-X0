b7781de4022ac53a21f0f00a18b79fa67bdfecca
stavrovskymax <stavrovsky96@yandex.ru>
2018-09-20 11:45:24 +0300

    Field - tests for the Point added

--- src/main/java/ru/lesson/X0/model/Field.java
+++ src/main/java/ru/lesson/X0/model/Field.java
@@ -1,22 +1,45 @@
 package ru.lesson.X0.model;
 
+import ru.lesson.X0.model.exceptions.AlreadyOccupiedException;
+import ru.lesson.X0.model.exceptions.InvalidPointException;
+
 import java.awt.*;
 
 public class Field {
 
     private static final int FIELD_SIZE = 3;
+    private static final int MAX_COORDINATE = FIELD_SIZE;
+    private static final int MIN_COORDINATE = 0;
+
     private final Figure[][] field = new Figure[FIELD_SIZE][FIELD_SIZE];
 
     public int getSize() {
         return FIELD_SIZE;
     }
 
-    // Try to make getLocation and setLocation in class Point
-    public Figure getFigure(final Point point) {
+    public Figure getFigure(final Point point) throws InvalidPointException {
+        if (!checkPoint(point)) {
+            throw new InvalidPointException();
+        }
         return field[point.x][point.y];
     }
 
-    public void setFigure(final Point point, final Figure figure) {
+    public void setFigure(final Point point, final Figure figure) throws InvalidPointException,
+                                                                        AlreadyOccupiedException {
+        if (!checkPoint(point)) {
+            throw new InvalidPointException();
+        }
+        if (field[point.x][point.y] != null) {
+            throw new AlreadyOccupiedException();
+        }
         field[point.x][point.y] = figure;
+    }
+
+    private boolean checkPoint(final Point point) {
+        return checkCoordinate(point.x) && checkCoordinate(point.y);
+    }
+
+    private boolean checkCoordinate(final int coordinate) {
+        return coordinate >= MIN_COORDINATE && coordinate < MAX_COORDINATE;
     }
 }
