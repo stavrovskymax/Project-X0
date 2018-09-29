ff2123e5bef160f76e963cb59b6e5c90c7d0e74e
stavrovskymax <stavrovsky96@yandex.ru>
2018-09-30 00:19:39 +0300

    WinnerController have been added methods for diagonal

--- src/test/java/ru/lesson/X0/controllers/WinnerControllerTest.java
+++ src/test/java/ru/lesson/X0/controllers/WinnerControllerTest.java
@@ -67,4 +67,72 @@ public class WinnerControllerTest {
             assertNull(actual);
         }
     }
+
+    @Test
+    public void testGetWinnerWhenWinnerDiagonal_1() throws Exception {
+        final WinnerController winnerController = new WinnerController();
+
+        final Field field = new Field(3);
+        field.setFigure(new Point(0,0), Figure.X);
+        field.setFigure(new Point(1,1), Figure.X);
+        field.setFigure(new Point(2,2), Figure.X);
+
+        final Figure expected = Figure.X;
+        final Figure actual = winnerController.getWinner(field);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetWinnerWhenNoWinnerDiagonal_1() throws Exception {
+        final WinnerController winnerController = new WinnerController();
+
+        final Field field = new Field(3);
+        field.setFigure(new Point(0,0), Figure.X);
+        field.setFigure(new Point(1,1), Figure.X);
+        field.setFigure(new Point(2,2), Figure.O);
+
+        final Figure actual = winnerController.getWinner(field);
+        assertNull(actual);
+    }
+
+    @Test
+    public void testGetWinnerWhenWinnerDiagonal_2() throws Exception {
+        final WinnerController winnerController = new WinnerController();
+
+        final Field field = new Field(3);
+        field.setFigure(new Point(0,2), Figure.X);
+        field.setFigure(new Point(1,1), Figure.X);
+        field.setFigure(new Point(2,0), Figure.X);
+
+        final Figure expected = Figure.X;
+        final Figure actual = winnerController.getWinner(field);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetWinnerWhenNoWinnerDiagonal_2() throws Exception {
+        final WinnerController winnerController = new WinnerController();
+
+        final Field field = new Field(3);
+        field.setFigure(new Point(0,2), Figure.X);
+        field.setFigure(new Point(1,1), Figure.X);
+        field.setFigure(new Point(2,0), Figure.O);
+
+        final Figure actual = winnerController.getWinner(field);
+        assertNull(actual);
+    }
+
+    @Test
+    public void testGetWinnerWhenNoWinnerRow_2() throws Exception {
+        final WinnerController winnerController = new WinnerController();
+
+        for (int i=0; i<3; i++) {
+            final Field field = new Field(3);
+            field.setFigure(new Point(i,0), Figure.O);
+            field.setFigure(new Point(i,1), Figure.X);
+            field.setFigure(new Point(i,2), Figure.X);
+            final Figure actual = winnerController.getWinner(field);
+            assertNull(actual);
+        }
+    }
 }
