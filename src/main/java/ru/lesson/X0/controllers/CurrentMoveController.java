885e71cdfe5ab9a6b2243101bd0b655f8aba9dd3
stavrovskymax <stavrovsky96@yandex.ru>
2018-09-30 01:03:26 +0300

    CurrentMoveController - created

---
+++ src/main/java/ru/lesson/X0/controllers/CurrentMoveController.java
@@ -0,0 +1,40 @@
+package ru.lesson.X0.controllers;
+
+import ru.lesson.X0.model.Field;
+import ru.lesson.X0.model.Figure;
+import ru.lesson.X0.model.exceptions.InvalidPointException;
+
+import java.awt.*;
+
+public class CurrentMoveController {
+
+    public Figure currentFigure(final Field field) {
+        int currentFigure = 0;
+
+        for (int x = 0; x < field.getSize(); x++) {
+            currentFigure = countFiguresInTheRow(field, x);
+        }
+
+        if (currentFigure == field.getSize() * field.getSize())
+            return null;
+
+        if (currentFigure % 2 == 0)
+            return Figure.X;
+
+        return Figure.O;
+    }
+
+    private int countFiguresInTheRow(final Field field, final int row) {
+        int currentFigure = 0;
+
+        for (int x = 0; x < field.getSize(); x++) {
+            try {
+                if (field.getFigure(new Point(x, row)) != null)
+                    currentFigure++;
+                } catch (final InvalidPointException e) {
+                    e.printStackTrace();
+                }
+            }
+        return currentFigure;
+    }
+}
