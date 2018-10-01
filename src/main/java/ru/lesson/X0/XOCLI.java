c82366b1a30a7b4efcd93a1d79d4478cbd52dfdc
stavrovskymax <stavrovsky96@yandex.ru>
2018-10-02 00:43:24 +0300

    XOCLI - Class launch (show empty field)

---
+++ src/main/java/ru/lesson/X0/XOCLI.java
@@ -0,0 +1,24 @@
+package ru.lesson.X0;
+
+import ru.lesson.X0.model.Field;
+import ru.lesson.X0.model.Figure;
+import ru.lesson.X0.model.Game;
+import ru.lesson.X0.model.Player;
+import view.ConsoleView;
+
+public class XOCLI {
+    public static void main(final String[] args) {
+        final String name1 = "Max";
+        final String name2 = "Slava";
+
+        final Player[] players = new Player[2];
+        players[0] = new Player(name1, Figure.X);
+        players[1] = new Player(name2, Figure.O);
+
+        final Game game = new Game(players, new Field(3), "X0");
+
+        final ConsoleView consoleView = new ConsoleView();
+        consoleView.show(game);
+
+    }
+}
