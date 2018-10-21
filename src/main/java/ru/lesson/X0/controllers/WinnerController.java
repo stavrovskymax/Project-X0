a045cefccd0acbcbdd96ebf0f6b94ba48f10eed9
stavrovskymax <stavrovsky96@yandex.ru>
2018-10-21 22:29:55 +0300

    WinnerController have been updated. Created lambda expressions

--- src/main/java/ru/lesson/X0/controllers/WinnerController.java
+++ src/main/java/ru/lesson/X0/controllers/WinnerController.java
@@ -8,60 +8,21 @@ import java.awt.*;
 
 public class WinnerController {
 
-    public Figure getWinner(final Field field) {
-        try {
-            for (int i=0; i<3; i++) {
-                if (check(field, new Point(i, 0), new IPointGenerator() {
-                    @Override
-                    public Point next(Point p) {
-                        return new Point(p.x, p.y + 1);
-                    }
-                }))
-                    return field.getFigure(new Point(i, 0));
-            }
+    public Figure getWinner(final Field field) throws InvalidPointException {
+        final int fieldSize = field.getSize();
 
-            for (int i=0; i<3; i++) {
-                if (check(field, new Point(0, i), new IPointGenerator() {
-                    @Override
-                    public Point next(Point p) {
-                        return new Point(p.x + 1, p.y);
-                    }
-                }))
-                    return field.getFigure(new Point(0, i));
-            }
+        for (int i = 0; i < fieldSize; i++) {
+            if (check(field, new Point(i, 0), p -> new Point(p.x, p.y + 1)))
+                return field.getFigure(new Point(i, 0));
 
-            for (int i=0; i<3; i++) {
-                if (check(field, new Point(0, 0), new IPointGenerator() {
-                    @Override
-                    public Point next(Point p) {
-                        return new Point(p.x + 1, p.y + 1);
-                    }
-                }))
-                    return field.getFigure(new Point(0, 0));
-            }
+            if (check(field, new Point(0, i), p -> new Point(p.x + 1, p.y)))
+                return field.getFigure(new Point(0, i));
 
-            for (int i=0; i<3; i++) {
-                if (check(field, new Point(0, 2), new IPointGenerator() {
-                    @Override
-                    public Point next(Point p) {
-                        return new Point(p.x + 1, p.y - 1);
-                    }
-                }))
-                    return field.getFigure(new Point(0, 2));
-            }
+            if (check(field, new Point(0, 0), p -> new Point(p.x + 1, p.y + 1)))
+                return field.getFigure(new Point(0, 0));
 
-                /*if (check(field, new Point(0, i), new Point(1, i), new Point(2, i)))
-                    return field.getFigure(new Point(0, i));
-            }
-
-                if (check(field, new Point(0, 0), new Point(1, 1), new Point(2, 2)))
-                    return field.getFigure(new Point(0, 0));
-
-                if (check(field, new Point(0, 2), new Point(1, 1), new Point(2, 0)))
-                    return field.getFigure(new Point(0, 2));*/
-
-        } catch (final InvalidPointException e) {
-            e.printStackTrace();
+            if (check(field, new Point(0, fieldSize - 1), p -> new Point(p.x + 1, p.y - 1)))
+                return field.getFigure(new Point(0, fieldSize - 1));
         }
         return null;
     }
@@ -76,25 +37,16 @@ public class WinnerController {
             currentFigure = field.getFigure(currentPoint);
             nextFigure = field.getFigure(nextPoint);
 
-            if (currentFigure == null) return false;
+            if (currentFigure == null)
+                return false;
         } catch (final InvalidPointException e) {
             return true;
         }
 
-        if (currentFigure != nextFigure) return false;
+        if (currentFigure != nextFigure)
+            return false;
 
         return check(field, nextPoint, pointGenerator);
-        /*try {
-            if (field.getFigure(p1) == null)
-                return false;
-            if (field.getFigure(p1) == field.getFigure(p2) &&
-                    field.getFigure(p1) == field.getFigure(p3))
-                return true;
-
-        } catch (InvalidPointException e) {
-            e.printStackTrace();
-        }
-        return false;*/
     }
 
     private interface IPointGenerator {
